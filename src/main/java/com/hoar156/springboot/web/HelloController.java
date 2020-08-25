7ba4336cc1d518f8a42b47d3c1f7565d37289a1f
hora156 <wostnek95@gmail.com>
2020-08-25 15:15:12 +0900

    어플리케이션, 컨트롤러, 컨트롤러 테스트, Dto 및 테스트 등록

---
+++ src/main/java/com/hoar156/springboot/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.hoar156.springboot.web;
+import com.hoar156.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return  new HelloResponseDto(name, amount);
+    }
+
+}
